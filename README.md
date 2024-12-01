61cbc9ada6c888704a88e835fbbe9b8c07e91e03
Andrey Hoglund <andrey.hoglund@protonmail.com>
2024-12-01 07:56:30 +0100

    day1

---
+++ README.md
@@ -0,0 +1,18 @@
+Advent of code 2024 solutions with R
+
+### File structure
+
+`bin/` holds the code for each day, `data/` holds the challenge inputs. Place your own inputs here and name them `dayX.txt`
+
+```
+|-- bin/
+|-- data/
+`-- README.md
+```
+
+
+<!--
+
+render code on front page (README). Github pages?
+
+-->
--- bin/day1.R
+++ bin/day1.R
@@ -1,1 +1,32 @@
 dat <- readLines("data/day1.txt")
+
+l1 <- sapply(dat, function(x){
+	x <- strsplit(x, " ")[[1]][1]
+	as.numeric(x)
+})
+l1 <- unname(l1)
+
+l2 <- sapply(dat, function(x){
+	x <- strsplit(x, " ")[[1]]
+	x <- x[length(x)]
+	as.numeric(x)
+})
+l2 <- unname(l2)
+
+l1 <- sort(l1)
+l2 <- sort(l2)
+
+# ~~ part 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+sum(abs(l1 - l2)) # 1341714
+
+
+# ~~ part 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+numbers <- l1[l1 %in% l2]
+
+out <- sapply(numbers, function(number){
+	fac <- sum(l2 %in% number)
+	number * fac
+})
+
+sum(out) # 27384707
